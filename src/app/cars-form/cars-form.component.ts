e8500f342cda8dfc741f7f575995edcf2e127da9
Alexey Egorov <Alexey.Egorov@emc.com>
2019-05-07 15:22:55 +0300

    add project

---
+++ src/app/cars-form/cars-form.component.ts
@@ -0,0 +1,46 @@
+import {Component} from '@angular/core'
+import {Store} from '@ngrx/store'
+import * as moment from 'moment'
+import {AppState} from '../redux/app.state'
+import {Car} from '../car.model'
+import {AddCar} from '../redux/cars.action'
+
+@Component({
+  selector: 'app-cars-form',
+  templateUrl: './cars-form.component.html',
+  styleUrls: ['./cars-form.component.css']
+})
+export class CarsFormComponent {
+
+  private id: number = 2
+
+  carName: string = ''
+  carModel: string = ''
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  onAdd() {
+    if (this.carModel === '' || this.carName === '') return
+
+    this.id = ++this.id
+
+    const car = new Car(
+      this.carName,
+      moment().format('DD.MM.YY'),
+      this.carModel,
+      false,
+      this.id
+    )
+
+    this.store.dispatch(new AddCar(car))
+
+    this.carModel = ''
+    this.carName = ''
+  }
+
+  onLoad() {
+    // todo
+  }
+
+}
---
+++ src/app/redux/cars.reducer.ts
@@ -0,0 +1,33 @@
+import {Car} from '../car.model'
+import {CAR_ACTION, CarsAction} from './cars.action'
+
+const initialState = {
+  cars: [
+    new Car('Ford', '12.12.12', 'Focus', false, 1),
+    new Car('Audi', '08.08.12', 'A4', false, 2)
+  ]
+}
+
+export function carsReducer(state = initialState, action: CarsAction) {
+  switch (action.type) {
+    case CAR_ACTION.ADD_CAR:
+      return {
+        ...state,
+        cars: [...state.cars, action.payload]
+      }
+    case CAR_ACTION.DELETE_CAR:
+      return {
+        ...state,
+        cars: [...state.cars.filter(c => c.id !== action.payload.id)]
+      }
+    case CAR_ACTION.UPDATE_CAR:
+      const idx = state.cars.findIndex(c => c.id === action.payload.id)
+      state.cars[idx].isSold = true
+      return {
+        ...state,
+        cars: [...state.cars]
+      }
+    default:
+      return state
+  }
+}
